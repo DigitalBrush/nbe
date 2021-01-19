36bda92e1ab02be9010fe4df6ea68508516a2b79
danstan <danstanobara@gmail.com>
2021-01-19 14:39:45 +0300

    qui sommes

--- page-templates/who.php
+++ page-templates/who.php
@@ -24,23 +24,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<main class="site-main" id="main" role="main">
 					<?php
 
-
-					if ( is_front_page() ) {
 						get_template_part( 'global-templates/who' ); 
 
-					}else{
-
-
-						while ( have_posts() ) {
-							the_post();
-							get_template_part( 'loop-templates/content', 'page' );
-
-							// If comments are open or we have at least one comment, load up the comment template.
-							if ( comments_open() || get_comments_number() ) {
-								comments_template();
-							}
-						}
-					}
+					
 					?>
 
 				</main><!-- #main -->
